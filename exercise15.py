8cff064eb98a535978d84a44b096ac2d04382ba3
Saravanan <57014443+bprathap104@users.noreply.github.com>
2020-10-15 22:44:20 +0530

    adding some more scripts

---
+++ exercise15.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, filename = argv
+txt = open(filename)
+
+print(f"Here's your filename {filename}:")
+print(txt.read())
+
+print(f"Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+print(txt_again.read())
